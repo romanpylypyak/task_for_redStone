f3c7befcbf387fbd97d59d9f922f0e65576d08d8
romanpylypyak <romanpylypyak97@gmail.com>
2021-03-24 23:01:24 +0200

    change front for handlebar feature

--- client/src/Components/User/User.js
+++ client/src/Components/User/User.js
@@ -24,6 +24,9 @@ class User extends React.Component {
     .then(res => {
       if(res.match(/Error/)){
         this.setState({errorHandler : res})
+      } else if(res.match(/html/)){
+        this.setState({postsCollection : "html"})
+        document.querySelector(".postsWrapper").innerHTML = res
       } else {
         res = JSON.parse(res)
         this.setState({postsCollection : res})
@@ -47,7 +50,7 @@ class User extends React.Component {
           <p>{this.state.errorHandler}</p>
         </div>
       )
-    } else {
+    } else if(typeof this.state.postsCollection === "object"){
       return (
         <div className="User">
           <h1>This is USER Page</h1>
@@ -57,6 +60,16 @@ class User extends React.Component {
           {this.state.postsCollection.map((item, i) => {
             return <div className={`postDiv postNumber${i+1}`} key={i}><p>{item.postHeader}</p><p>{item.postText}</p></div>
           })}
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="User">
+          <h1>This is USER Page</h1>
+          <p>Hello {this.props.location.state.data.user.name} !</p>
+          <h2>Your Posts</h2>
+          <div className="postsWrapper">
           </div>
         </div>
       );

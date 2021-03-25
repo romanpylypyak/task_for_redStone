2262bfdf969f48e228a1c80577c3ca1f1efb14b4
romanpylypyak <romanpylypyak97@gmail.com>
2021-03-25 20:14:08 +0200

    nth imp

--- client/src/Components/User/User.js
+++ client/src/Components/User/User.js
@@ -14,7 +14,7 @@ class User extends React.Component {
     let data = {
     "name": localStorage.getItem("user")
     }
-    fetch('http://localhost:5000/user', {
+    fetch('http://localhost:5000/api/user', {
     method: "POST",
     headers: {
         "Content-Type": "application/json",
